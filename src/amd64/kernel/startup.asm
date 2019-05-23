2f25e0de11735150bb376316efef565c1680b6fc
Nathan Ringo <remexre@gmail.com>
2019-05-22 22:13:49 -0500

    ...

---
+++ src/amd64/kernel/startup.asm
@@ -0,0 +1,32 @@
+bits 64
+
+extern interpret
+
+global forth_cold
+
+[section .text]
+
+; Performs a cold startup of the Forth system.
+forth_cold:
+	xchg bx, bx
+	mov r8, forth_startup
+	mov r9, forth_startup.len
+	call interpret
+	xchg bx, bx
+	mov r8, forth_std
+	mov r9, forth_std.len
+	call interpret
+	xchg bx, bx
+	ret
+
+[section .forth_code]
+
+forth_startup:
+	incbin "src/amd64/forth/startup.f"
+.len equ $-forth_startup
+
+forth_std:
+	incbin "src/amd64/forth/std.f"
+.len equ $-forth_std
+
+; vi: cc=80 ft=nasm
