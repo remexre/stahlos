fe92c188edd2ec8789656479f696986f878b3702
Nathan Ringo <remexre@gmail.com>
2019-07-06 19:55:32 -0700

    ...

--- src/amd64/forth/init.f
+++ src/amd64/forth/init.f
@@ -12,18 +12,34 @@
     I 4 + D@ ALIGN-TO-CELL
   +LOOP DROP ;
 
-:NONAME ?( addr -- flag)
-  DUP D@ 6 = IF
-    . TRUE
-  ELSE
-    DROP FALSE
-  THEN ; DROP \ mb2-each-tag
+: mmap-entry-base-addr @ ;
+: mmap-entry-length 8 + @ ;
+: mmap-entry-type $10 + D@ ;
+: add-free-list-link
+  DUP mmap-entry-type 1 <> IF RETURN THEN
+  .S
+  DUP ." link at 0x" . CR
+  DUP ."    from 0x" mmap-entry-base-addr . CR
+  DUP ."      to 0x" DUP mmap-entry-base-addr SWAP mmap-entry-length + . CR
+  DUP ." with type " mmap-entry-type . CR CR
+  .S DROP ;
 
-:NONAME 5 0 DO I . LOOP ; EXECUTE
-.( going from first to second test)
-:NONAME 5 0 DO I . I 3 = IF DEBUG LEAVE THEN LOOP ; EXECUTE
+: mmap-start-addr $10 + ;
+: mmap-end-addr DUP mmap-start-addr SWAP 4 + D@ $10 - + ;
+: mmap-entry-size 8 + D@ ;
+: build-free-list
+  ." mmap tag at 0x" DUP . CR
+  >R \ Yeah, this is unidiomatic as heck.
+  R@ DROP
+  R@ mmap-end-addr
+  R@ mmap-start-addr
+  R> mmap-entry-size -ROT ?DO
+    DUP I add-free-list-link
+  +LOOP BP DROP ;
+
+:NONAME DUP D@ 6 = IF build-free-list TRUE ELSE DROP FALSE THEN ; mb2-each-tag
 
 .( Done with init.f!)
-QUIT
+BP QUIT
 
 \ vim: set cc=80 ft=forth ss=2 sw=2 ts=2 et :
