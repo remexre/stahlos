473d69127ae3b626f96de6967375e444307edacf
Nathan Ringo <remexre@gmail.com>
2019-10-10 23:15:43 -0500

    ...

--- src/hosted_compiler/tyck.ml
+++ src/hosted_compiler/tyck.ml
@@ -1,20 +1,5 @@
 open State_monad
 
-type expr =
-  { type_ : expr_inner
-  ; value : expr_inner
-  }
-
-and expr_inner
-  = App of expr * expr
-  | Global of string
-  | Lam of expr
-  | Lit of string * Sexpr.t
-  | Logic of int
-  | Local of int
-  | Pi of expr * expr
-  | Universe of int
-
 exception Type_error of expr * expr
 
 type ctx_entry =
---
+++ src/hosted_compiler/uast.mli
@@ -0,0 +1,16 @@
+type expr =
+  { type_ : expr_inner
+  ; value : expr_inner
+  }
+
+and expr_inner
+  = App of expr * expr
+  | Global of string
+  | Lam of expr
+  | Lit of string * Sexpr.t
+  | Logic of int
+  | Local of int
+  | Pi of expr * expr
+  | Universe of int
+
+val from_ast : Ast.expr -> expr  
